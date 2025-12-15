568bb3d62ab8f17ad533e20c4cf81e436fc0254e
fatimah_here <fatimahchudhary910@gmail.com>
2025-12-15 18:49:56 +0500

    updated

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,9 +1,17 @@
 import Navbar from "../components/layout/Navbar";
+import TopHeader from "../components/common/header";
+
 import { Container } from "@mui/material";
 
 export default function UserLayout({ children }) {
   return (
+
     <>
+      <>
+        <TopHeader />
+        {/* Main Navbar below */}
+      </>
+
       <Navbar />
       <Container sx={{ mt: 4 }}>{children}</Container>
     </>
